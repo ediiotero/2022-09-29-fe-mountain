7d0e1d5abf74f6e3a318da0e17715fc546b463c6
ediiotero <e.otero81@gmail.com>
2022-11-03 20:35:08 -0400

    js week 5 ready

---
+++ JS-week-5/demo.js
@@ -0,0 +1,5 @@
+//https://www.youtube.com/watch?v=khuDeNwXkfI
+//https://www.youtube.com/watch?v=ZYa_NiOUTQo
+//https://www.youtube.com/watch?v=jM0WcyQWMSM
+
+
---
+++ JS-week-5/index.js
@@ -0,0 +1,35 @@
+// Using the object below
+// 1 console log the customer name
+// 2 console log using a template literal "shipping to 123 Main street Gotham NY 12345" 
+// 3 add a property of "orderNum" and set it to 111222, change age to 44, console.log the object
+
+const customer = {
+    name: "Toby",
+    age: 32,
+    address: {
+        street: "123 Main street",
+        city: "Gotham",
+        state: "NY",
+        zip: "12345"
+    }
+}
+
+
+
+// ==================================
+
+// Write a class for a Hero 
+// - the Hero should have a property of name, health, and power
+// - the Hero should also have a method of attack 
+//   - attack should return power plus 5
+
+// Write another class that will inherit Hero and call it Villain
+// - the villain should have all the same properties as Hero
+// - add a method call magic
+//   - magic should return power multiplied by 2
+
+// Create a hero and a villain and log out all of their properties
+
+// *Bonus - Write a loop that will have the hero(use attack) and villain(use magic) attack each other 
+//          reducing the amount of health left and console.log each round until health is at 0.
+
