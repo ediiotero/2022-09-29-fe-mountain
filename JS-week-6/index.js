6b55c040c30c634aec528d1e4f39a116522808bd
ediiotero <e.otero81@gmail.com>
2022-11-10 22:34:45 -0500

    js week 6 done

--- JS-week-6/index.js
+++ JS-week-6/index.js
@@ -4,3 +4,61 @@
 // and another to convert celsius to kelvin->   celsius + 273.15
 // create 2 new object with this class
 
+class Temperature {
+    constructor(fahrenheit) {
+        if(fahrenheit <= 212 && fahrenheit >= -459.67) {
+            this.fahrenheit = fahrenheit
+        }else {
+            console.error(`${fahrenheit} is not a valid fahrenheit temperature`)
+        }
+    }
+
+    celsius() {
+        const fahrenheitToC = ((this.fahrenheit - 32)*5)/9
+        return fahrenheitToC
+    }
+
+    kelvin() {
+        const celsius = this.celsius()
+        return celsius + 273.15
+    }
+}
+
+const temp = new Temperature(100)
+
+console.log(temp)
+console.log(temp.celsius().toFixed(2))
+console.log(temp.kelvin().toFixed(2))
+
+
+const players = ['Eddie', 'Adam', 'Jennifer', 'Stephanie', 'Cody']
+const positions = ['captain', 'guard', 'forward', 'center']
+
+const teams = [];
+
+positions.forEach(position => {
+    players.map(player => teams.push({player, position}))
+})
+
+console.log(teams)
+
+function shuffle(array) {
+    const teamOne = []
+    const teamTwo = []
+
+    while(array.length > 0) {
+        const random1 = Math.floor(Math.random() * array.length)
+        const getItem1 = array.splice(random1,1)
+        const random2 = Math.floor(Math.random() * array.length)
+        const getItem2 = array.splice(random2,1)
+
+        teamOne.push(getItem1)
+        teamTwo.push(getItem2)
+    }
+    
+    return [teamOne.flat(), teamTwo.flat()]
+}
+
+const deck = shuffle(teams)
+console.log(deck[0])
+console.log(deck[1])
--- JS-week-6/test.js
+++ JS-week-6/test.js
@@ -17,3 +17,70 @@ describe("myFunctions", () => {
   });
 });
 
+function isTheSame(input1, input2) {
+  return input1 === input2
+}
+
+describe('isTheSame', () => {
+  it('should return true if both inputs are the same', () => {
+    const result = isTheSame(1,1)
+    expect(result).to.be.true;
+  })
+
+  it('should return false if both inputs are not the same', () => {
+    const result = isTheSame(1,2)
+    expect(result).to.be.false;
+  })
+})
+
+function getSum(a,b) {
+  return a+b
+}
+
+describe('getSum', () => {
+  it('should return the sum of two inputs', () => {
+    const sum = getSum(2,3)
+    expect(sum).to.equal(5)
+  })
+
+  it('should concatenate strings', () => {
+    const sum = getSum('hello', ' world')
+    expect(sum).to.equal('hello world')
+  })
+})
+
+const nameAry = ['tom', 'jerry'];
+
+describe('name array', () => {
+    it('should have a length of 2', () => {
+        expect(nameAry).to.have.length(2);
+    })
+})
+
+const player1 = {card: 'king', value: 10}
+const player2 = {card: 'heart', value: 5}
+const player3 = {card: 'spade', value: 5}
+
+function war(p1, p2) {
+  if(p1.value > p2.value){
+    return `${p1.card} card wins`;
+  }else if (p1.value < p2.value) {
+    return `${p2.card} card wins`;
+  }
+  return 'its a tie';
+}
+
+describe('Playing War', () => {
+  it('should return king card wins', () => {
+    const winner = war(player1, player2);
+    expect(player1.value).to.be.greaterThan(player2.value)
+    expect(winner).to.equal('king card wins')
+  })
+
+  it('should return its a tie', () => {
+    const winner = war(player2, player3)
+    expect(player2.value).to.equal(player3.value)
+    expect(winner).to.equal('its a tie')
+  })
+
+})
