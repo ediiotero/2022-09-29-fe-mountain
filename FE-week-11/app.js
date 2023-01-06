bb94b89f09a4ea777ba6f1ae72edced03ef66b85
ediiotero <e.otero81@gmail.com>
2023-01-05 20:27:50 -0500

    week 11 ready

---
+++ FE-week-11/app.js
@@ -0,0 +1,5 @@
+// Build a form with a title and a body input 
+    // when pressing submit the form should make a post request to 
+    // https://jsonplaceholder.typicode.com/posts
+    // console.log the response
+    // the API should respond with your title and body and an id: 101
